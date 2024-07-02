c0c084de97ac9b6e5e0ce4b23ec347c581b85697
Shovan Nag <shovanr06@gmail.com>
2024-07-02 23:33:48 +0600

    1st commit

---
+++ p2-data/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+   <div>
+        <h1>Welcome to VueJs and learning!</h1>
+        <h2>Today is 07/02/2024</h2>
+    </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
